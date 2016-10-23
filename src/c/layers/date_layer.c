8a3d41be1aef0b65da4b7ca8af5175f3dd1033a8
Jan Hoffmann <jan.christian.hoffmann@gmail.com>
2016-10-23 22:21:32 +0200

    Add layer displaying for displaying a date
    Draws a specified date using the character graphics functions.

---
+++ src/c/layers/date_layer.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2016 Jan Hoffmann
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "date_layer.h"
+#include "../graphics/character.h"
+#include "../lib/sizes.h"
+
+
+typedef struct {
+  uint16_t year;
+  uint8_t month;
+  uint8_t day;
+} DateLayerData;
+
+
+void date_layer_set_date(DateLayer* date_layer, uint16_t year, uint8_t month, uint8_t day) {
+  DateLayerData *data = (DateLayerData *)layer_get_data(date_layer);
+  if (year <= 9999 && month <= 12 && day <= 31) {
+    data->year = year;
+    data->month = month;
+    data->day = day;
+    layer_mark_dirty(date_layer);
+  }
+}
+
+static void date_layer_update_proc(Layer *layer, GContext *ctx) {
+  DateLayer *date_layer = (DateLayer *)layer;
+  DateLayerData *data = (DateLayerData *)layer_get_data(date_layer);
+
+  graphics_context_set_fill_color(ctx, GColorBlack);
+
+  size_t length = 10;
+  Character characters[10] = {*CHARACTER_NUMBERS[data->year / 1000],
+                              *CHARACTER_NUMBERS[(data->year / 100) % 10],
+                              *CHARACTER_NUMBERS[(data->year / 10) % 10],
+                              *CHARACTER_NUMBERS[data->year % 10],
+                              CHARACTER_HYPHEN,
+                              *CHARACTER_NUMBERS[data->month / 10],
+                              *CHARACTER_NUMBERS[data->month % 10],
+                              CHARACTER_HYPHEN,
+                              *CHARACTER_NUMBERS[data->day / 10],
+                              *CHARACTER_NUMBERS[data->day % 10]};
+
+  graphics_draw_character_array(ctx, GPoint(0, 0), characters, length);
+}
+
+DateLayer* date_layer_create(GRect frame) {
+  DateLayer *date_layer = layer_create_with_data(frame, sizeof(DateLayerData));
+  layer_set_update_proc(date_layer, date_layer_update_proc);
+  layer_mark_dirty(date_layer);
+  return date_layer;
+}
+
+void date_layer_destroy(DateLayer* date_layer) {
+  if (date_layer) {
+    layer_destroy(date_layer);
+  }
+}
---
+++ src/c/layers/date_layer.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016 Jan Hoffmann
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+
+#include <pebble.h>
+
+
+typedef Layer DateLayer;
+
+DateLayer* date_layer_create(GRect frame);
+void date_layer_destroy(DateLayer* date_layer);
+void date_layer_set_date(DateLayer* date_layer, uint16_t year, uint8_t month, uint8_t day);
